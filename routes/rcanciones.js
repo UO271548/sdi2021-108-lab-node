07e109f840cc63289faee67d7037f13a7cad914e
UO271548 <uo271548@uniovi.es>
2021-04-12 19:08:07 +0200

    SDI-2021-108-9.2-Redirección de peticiones.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -75,7 +75,8 @@ module.exports = function(app, swig, gestorBD) {
                                     if (err) {
                                         res.send("Error al subir el audio");
                                     } else {
-                                        res.send("Agregada id: "+ id);
+                                        res.redirect("/publicaciones");
+
                                     }
                                 });
                             }
@@ -179,7 +180,7 @@ module.exports = function(app, swig, gestorBD) {
                     if( result == null){
                         res.send("Error en la modificación");
                     } else {
-                        res.send("Modificado");
+                        res.redirect("/publicaciones");
                     }
                 });
             }
