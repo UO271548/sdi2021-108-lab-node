e889d8383c7886e52f544f6e16b42fefcd9d8eb7
UO271548 <uo271548@uniovi.es>
2021-04-20 18:34:59 +0200

    SDI-2021-108-10.2-SW API REST Autenticación.

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -18,7 +18,7 @@ module.exports = function(app, gestorBD) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
 
         gestorBD.obtenerCanciones(criterio,function(canciones){
-            if ( canciones == null ){
+            if ( canciones == null){
                 res.status(500);
                 res.json({
                     error : "se ha producido un error"
@@ -97,6 +97,37 @@ module.exports = function(app, gestorBD) {
             }
         });
     });
+
+    app.post("/api/autenticar/" , function (req, res) {
+
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
+
+        let criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function (usuarios){
+            if (usuarios ==  null || usuarios.length == 0){
+                res.status(401);
+                res.json({
+                    autenticado : false
+                })
+            }else{
+                let token = app.get('jwt').sign(
+                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    "secreto");
+
+                res.status(200);
+                res.json({
+                    autenticado : true,
+                    token : token
+
+                })
+            }
+        });
+    });
+
 
 
 }
